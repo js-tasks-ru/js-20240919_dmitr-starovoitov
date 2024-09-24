6a5a27f2983bf11a477f740729f0ac7a4f16022d
Dmitry Starovoytov <>
2024-09-24 20:15:53 +0300

    completed omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  if ([...fields].length !== 0) {
+    return Object.entries(obj).reduce((obj, current) => {
+      if (![...fields].includes(current[0])) {
+        obj[current[0]] = current[1];
+      }
+      return obj;
+    }, {});
+  }
+  return {};
 };
