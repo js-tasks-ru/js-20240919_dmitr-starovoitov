9512f44e45f7ea08c62ed5a205ad8acf00114940
Dmitry Starovoytov <>
2024-10-15 17:20:16 +0300

    updated columnChart: added enable / disable loader methods. On creating loading state depends on data size. There is no loading state after updating even for empty response.

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -6,7 +6,7 @@ const ORDERS_PATH = "/api/dashboard/orders";
 
 export default class ColumnChartV2 extends ColumnChartV1 {
   async update(startDate, endDate) {
-    this.setChartLoadingState(true);
+    this.enableLoader();
 
     const url = new URL(ORDERS_PATH, BACKEND_URL);
     url.pathname;
@@ -17,14 +17,8 @@ export default class ColumnChartV2 extends ColumnChartV1 {
 
     super.update(Object.values(responseData));
 
-    return responseData;
-  }
+    this.disableLoader();
 
-  setChartLoadingState(isLoading) {
-    if (!isLoading) {
-      this.element.classList.remove("column-chart_loading");
-    } else {
-      this.element.classList.add("column-chart_loading");
-    }
+    return responseData;
   }
 }
